c8f41c6a117ce37af91a8cd1edc8e05c17659494
dreamxian4 <122443241+dreamxian4@users.noreply.github.com>
2023-06-26 20:22:37 +0800

    游戏部分完成（有bug版）

---
+++ werewolf/AudioApi/audio_common.h
@@ -0,0 +1,23 @@
+#ifndef AUDIO_COMMON_H
+#define AUDIO_COMMON_H
+
+#include<QAudioInput>
+#include<QAudioOutput>
+#include<QAudioFormat>
+#include<QAudioDeviceInfo>
+#include<QMessageBox>
+#include<qDebug>
+//使用speex
+
+extern "C"{
+#include"WebRtc_Vad/webrtc_vad.h"
+//#include"speex/include/speex.h"
+}
+
+//#define USE_SPEEX 1
+#define USE_VAD 1
+
+//设置压缩质量
+#define SPEEX_QUALITY (8)
+
+#endif // AUDIO_COMMON_H
